1ea9d6cec1cd353869d7ccccd6870e6ec710751c
ManuelCebreiro <cebreirom@gmail.com>
2022-07-10 16:28:08 +0000

    Proyecto listo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,31 +1,27 @@
 import { func } from "prop-types";
 import React, { useState } from "react";
 
-
 const Home = () => {
 
 	const [inputText, setInputText] = useState([]);
-	
-const borrar = ()=>{
-	e.target.parentElement.style.display = none
-	 console.log(e.target.value)
-}
+	const [inputValue, setinputValue] = useState();
 
 	return (
 	<div className="container">
 		<h1>Todo</h1>
-		<input type="text" 	onKeyDown={(e)=>{
-							if( e.key === "Enter"){
-							setInputText([...inputText, e.target.value])
-							
-							}
+		<input type="text" value={inputValue} onChange={(e)=>setinputValue(e.target.value)} onKeyDown={(e)=>{
 
+							let array = Array.from(e.target.value);
+							let filterarray = array.filter(words => words !== " ");
+							
+							if( e.key === "Enter" &&  filterarray.length){
+							setInputText([...inputText, e.target.value]);
+							setinputValue("")}
 							}}
 							className="form-control" 
 							placeholder="Username" aria-label="Username" 
 							aria-describedby="basic-addon1"/>
 		<ul>
-
 		{inputText.map((texto,index)=>{return(
 			<li key={index}>{texto}
 					<button 
