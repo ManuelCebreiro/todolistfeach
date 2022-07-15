4c3ef44cbd69747dbe6183e69bb3f8c14a63c685
ManuelCebreiro <cebreirom@gmail.com>
2022-07-15 19:37:13 +0000

    arreglando

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,10 +4,13 @@ import { func } from "prop-types";
 
 const Home = () => {
 
-	const url = 'https://assets.breatheco.de/apis/fake/todos/user/man';
+	const url = 'https://assets.breatheco.de/apis/fake/todos/user/manuelcebreiro';
 
 	const [inputText, setInputText] = useState([]);
-	const [inputValue, setinputValue] = useState();
+	const [inputValue, setinputValue] = useState();   //para vaciar el input
+	const [deleteall, setdeleteall] = useState();
+	
+	
 
 
 	const get = ()=>{
@@ -17,6 +20,9 @@ const Home = () => {
 			"Content-Type": "application/json"
 		  }
 		})
+		// .then(response => {
+		// 	if(!response.ok)
+		// 		newUser()})
 		.then(resp => {return resp.json();})
 		.then(data => 
 		setInputText(data))
@@ -26,32 +32,32 @@ const Home = () => {
 	
 		}
 		);}
-	
-
 
 useEffect(() => {
 	get()
   },[]);
 
-const newUser = () =>{
-fetch(url,{
-	method: "GET",
-	body: JSON.stringify([]),    //con esto creo un nuevo usuario. Porque me piden un array vacio para crearlo
-	headers: {
-	  "Content-Type": "application/json"
-	}
-})
-}
+// const newUser = () =>{
+// fetch(url,{
+// 	method: "GET",
+// 	body: JSON.stringify([]),    //con esto creo un nuevo usuario. Porque me piden un array vacio para crearlo
+// 	headers: {
+// 	  "Content-Type": "application/json"
+// 	}
+// })
+// }
 
-const actualizarlista = () =>{
+const actualizarlista = (data) =>{
+	console.log(data)
 fetch(url,{
 	method: "PUT",
 	headers: {
 		"Content-Type": "application/json"
 	  },
-	body: JSON.stringify([...inputText, {label :inputValue, done : false}]),
+	body: JSON.stringify(data),
 	})
 .then(respuesta => {
+	console.log(respuesta.status)
 	if(respuesta.ok)
 	get()
 })
@@ -59,44 +65,45 @@ fetch(url,{
 	console.log("no funciona hay un error")
 	)
 }
+// const deletelist = () =>{
+// 	fetch(url,{
+// 		method: "DELETE",
+// 		body: JSON.stringify(),    
+// 		headers: {
+// 		  "Content-Type": "application/json"
+// 		}
+// 	})
+// 	}
 
 
-
-
-// .then(reponse => {
-// 	if (!reponse.ok)
-// 		throw new Error ("No existe el usuario")
-// })
-// .then(data =>{
-// 	setInputText([...data])
-// 	console.log("Se han descargado las tareas")
-// })
-// .catch(error=>
-// 	console.log(error)
-// 	newUser()
-// 	)
-
-
+function borrarelemento(why){
+	const listaborrar = inputText.filter(valor=> valor !== why )
+	actualizarlista(listaborrar)
+}
 
 
 	return (
 		<div className="container text-center">
 			<h1 className="container d-flex justify-content-center text-center col-8 bi bi-x-lg">TODOS</h1>
-			<div className="container d-flex justify-content-center text-center mb-3"><button type="button" className="btn btn-danger">Borrar</button></div>
+			<div className="container d-flex justify-content-center text-center mb-3">
+				<button type="button" className="btn btn-danger" value>Borrar</button></div>
 			<div className="container shadow p-1 bg-white rounded col-8 bi bi-x-lg">
 			<input type="text" value={inputValue} onChange={(e) => setinputValue(e.target.value)} 
-			
 				onKeyDown={(e) => {
-
+				
+				let newObj = {label : inputValue, done : false};
+				let aux= [...inputText, newObj]
+				
 				let array = Array.from(e.target.value);
 				let filterarray = array.filter(words => words !== " ");
 
 				if (e.key === "Enter" && filterarray.length) {
-					actualizarlista()					
-					setInputText([...inputText, {label : e.target.value, done : false}]);
+					actualizarlista(aux)					
+					setInputText(aux);
 					setinputValue("")
 				}
 			}}
+
 				className="form-control"
 				placeholder="¿Qué tienes que hacer?" aria-label="Username"
 				aria-describedby="basic-addon1" />
@@ -111,7 +118,7 @@ fetch(url,{
 							<button
 								type="button"
 								className="btn-close "
-								onClick={(e) => e.target.parentElement.style.display = "none"}></button></li>
+								onClick={()=>borrarelemento(texto)}></button></li>
 
 					)
 				})}
