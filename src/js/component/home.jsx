c467e8ba1c94248b8762000afa6c6a2fa9ae9787
ManuelCebreiro <cebreirom@gmail.com>
2022-07-21 22:48:30 +0000

    modificado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,123 +9,133 @@ const Home = () => {
 	const [inputText, setInputText] = useState([]);
 	const [inputValue, setinputValue] = useState();   //para vaciar el input
 	// const [deleteall, setdeleteall] = useState();
-	
-	
 
 
-	const get = ()=>{
+
+
+	const get = () => {
 		fetch(url, {
-		  method: "GET",
-		  headers: {
-			"Content-Type": "application/json"
-		  }
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json"
+			}
 		})
-		// .then(response => {
-		// 	if(!response.ok)
-		// 		newUser()})
-		.then(resp => {return resp.json();})
-		.then(data => 
-		setInputText(data))
-		.catch(error => {
-			//manejo de errores
-			console.log(error)
-	
-		}
-		);}
+		
+			.then(resp => { return resp.json(); })
+			.then(data =>
+				setInputText(data))
+			.catch(error => {
+				//manejo de errores
+				console.log(error)
 
-useEffect(() => {
-	get()
-  },[]);
+			}
+			);
+	}
+	const newUser = () => {
+		fetch(url, {
+			method: "POST",
+			body: JSON.stringify([]),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+		.then(respuesta =>{
+			if(respuesta.ok)
+			get()
+		})
+		.catch(error=>
+			console.log("No se ha podido llamar a los datos"))
+	}
 
-// const newUser = () =>{
-// fetch(url,{
-// 	method: "GET",
-// 	body: JSON.stringify([]),    //con esto creo un nuevo usuario. Porque me piden un array vacio para crearlo
-// 	headers: {
-// 	  "Content-Type": "application/json"
-// 	}
-// })
-// }
+	useEffect(() => {
+		get()
+	}, []);
 
-const actualizarlista = (data) =>{
-	console.log(data)
-fetch(url,{
-	method: "PUT",
-	headers: {
-		"Content-Type": "application/json"
-	  },
-	body: JSON.stringify(data),
-	})
-.then(respuesta => {
-	console.log(respuesta.status)
-	if(respuesta.ok)
-	get()
-})
-.catch(error=> 
-	console.log("no funciona hay un error")
-	)
-}
-// const deletelist = () =>{
-// 	fetch(url,{
-// 		method: "DELETE",
-// 		body: JSON.stringify(),    
-// 		headers: {
-// 		  "Content-Type": "application/json"
-// 		}
-// 	})
-// 	}
+	const actualizarlista = (data) => {
+		console.log(data)
+		fetch(url, {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(data),
+		})
+			.then(respuesta => {
+				console.log(respuesta.status)
+				if (respuesta.ok)
+					get()
+			
+			})
+			.catch(error =>
+				console.log("no funciona hay un error")
+			)
+	}
+	const deletelist = () => {
+		fetch(url, {
+			method: "DELETE",
+			body: JSON.stringify(),
+			headers: {
+				"Content-Type": "application/json"
+			}})
+			.then(response => {
+				if(response.ok)
+					newUser()
+					
+			})
+		
+	}
 
 
-function borrarelemento(why){
-	const listaborrar = inputText.filter(valor=> valor !== why )
-	actualizarlista(listaborrar)
-}
+	function borrarelemento(why) {
+		const listaborrar = inputText.filter(valor => valor !== why)
+		actualizarlista(listaborrar)
+	}
 
 
 	return (
 		<div className="container text-center">
 			<h1 className="container d-flex justify-content-center text-center col-8 bi bi-x-lg">TODOS</h1>
 			<div className="container d-flex justify-content-center text-center mb-3">
-				{/* <button type="button" className="btn btn-danger" value>Borrar</button> */}
-				</div>
-			<div className="container shadow p-1 bg-white rounded col-8 bi bi-x-lg">
-			<input type="text" value={inputValue} onChange={(e) => setinputValue(e.target.value)} 
-				onKeyDown={(e) => {
-				
-				let newObj = {label : inputValue, done : false};
-				let aux= [...inputText, newObj]
-				
-				let array = Array.from(e.target.value);
-				let filterarray = array.filter(words => words !== " ");
-
-				if (e.key === "Enter" && filterarray.length) {
-					actualizarlista(aux)					
-					setInputText(aux);
-					setinputValue("")
-				}
-			}}
-
-				className="form-control"
-				placeholder="¿Qué tienes que hacer?" aria-label="Username"
-				aria-describedby="basic-addon1" />
-
-		<div className="text-center">
-			<ul className="list-group">
-				{inputText.map((texto, index) => {
-					return (
-						<li key={index}
-						// [{"label":"sample task","done":false}]
-							className="container list-group-item">{texto.label}   
-							<button
-								type="button"
-								className="btn-close "
-								onClick={()=>borrarelemento(texto)}></button></li>
-
-					)
-				})}
-			</ul>
+				<button type="button" onClick={() => deletelist()} className="btn btn-danger" value>Borrar</button>
 			</div>
-		</div>
+			<div className="container shadow p-1 bg-white rounded col-8 bi bi-x-lg">
+				<input type="text" value={inputValue} onChange={(e) => setinputValue(e.target.value)}
+					onKeyDown={(e) => {
+
+						let newObj = { label: inputValue, done: false };
+						let aux = [...inputText, newObj]
+
+						let array = Array.from(e.target.value);
+						let filterarray = array.filter(words => words !== " ");
+
+						if (e.key === "Enter" && filterarray.length) {
+							actualizarlista(aux)
+							setInputText(aux);
+							setinputValue("")
+						}
+					}}
+
+					className="form-control"
+					placeholder="¿Qué tienes que hacer?" aria-label="Username"
+					aria-describedby="basic-addon1" />
+
+				<div className="text-center">
+					<ul className="list-group">
+						{inputText.map((texto, index) => {
+							return (
+								<li key={index}
+									// [{"label":"sample task","done":false}]
+									className="container list-group-item">{texto.label}
+									<button
+										type="button"
+										className="btn-close "
+										onClick={() => borrarelemento(texto)}></button></li>
+
+							)
+						})}
+					</ul>
+				</div>
+			</div>
 		</div>
 	);
 };
